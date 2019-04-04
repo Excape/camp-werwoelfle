4cdc221dcff13cbfb74048f0266ea72d39b656c8
Suter, Robin <robin.suter@zuehlke.com>
2019-04-04 21:38:39 +0200

    upgrade to JUnit5 and new gradle version

--- backend/src/test/kotlin/ch/zuehlke/camp/werewolf/controllers/TestControllerTest.kt
+++ backend/src/test/kotlin/ch/zuehlke/camp/werewolf/controllers/TestControllerTest.kt
@@ -1,19 +1,20 @@
 package ch.zuehlke.camp.werewolf.controllers
 
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
-import org.springframework.test.context.junit4.SpringRunner
+import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-@RunWith(SpringRunner::class)
-@WebMvcTest(value = [TestController::class])
+@ExtendWith(SpringExtension::class)
+@WebMvcTest(TestController::class)
 class TestControllerTest {
+
     @Autowired
     private val mockMvc: MockMvc? = null
 
