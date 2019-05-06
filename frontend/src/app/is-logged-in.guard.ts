46475fb4209764ee0df21f7392b0075d36167328
Trentini, Muriele <muriele.trentini@zuehlke.com>
2019-05-06 17:19:57 +0200

    add logic to guard -> redirect to login if not already logged in

--- frontend/src/app/is-logged-in.guard.ts
+++ frontend/src/app/is-logged-in.guard.ts
@@ -1,14 +1,28 @@
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IsLoggedInGuard implements CanActivate {
+  constructor(private router: Router) {
+  }
+
   canActivate(
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return true;
+
+    if (sessionStorage.getItem("profile")) {
+      return true;
+    } else {
+      this.router.navigate(['/login'], {
+        queryParams: {
+          return: state.url
+        }
+      });
+      return false;
+    }
+
   }
 }
