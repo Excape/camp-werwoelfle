20b0884415e14c8e16216ca23a932fef865eaeb4
sdiegas <stefan.diegas@zuehlke.com>
2019-05-08 15:27:32 +0200

    fixed Tests

--- frontend/src/app/gamecontent/voting/voting.service.spec.ts
+++ frontend/src/app/gamecontent/voting/voting.service.spec.ts
@@ -1,9 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 
 import { VotingService } from './voting.service';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('VotingService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({imports: [HttpClientTestingModule, RouterTestingModule]}));
 
   it('should be created', () => {
     const service: VotingService = TestBed.get(VotingService);
