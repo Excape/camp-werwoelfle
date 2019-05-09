48428dbf4fb1b8828a2490551fedf728981d41ba
Suter, Robin <robin.suter@zuehlke.com>
2019-05-09 08:38:53 +0200

    make frontend finally compile

--- frontend/src/app/profile/edit-profile/edit-profile.component.spec.ts
+++ frontend/src/app/profile/edit-profile/edit-profile.component.spec.ts
@@ -1,7 +1,6 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {EditProfileComponent} from './edit-profile.component';
-import {MatCardModule} from "@angular/material";
 import {MaterialModule} from "../../material/material.module";
 import {FormsModule} from "@angular/forms";
 import {RouterTestingModule} from "@angular/router/testing";
