2c0699f5018e84de8646120b1d39d6856ac4a226
alinaliburkina <alina.liburkina@gmail.com>
2019-05-10 11:28:20 +0200

    add default avatar

--- backend/src/main/kotlin/ch/zuehlke/camp/werewolf/controllers/PlayerProfileController.kt
+++ backend/src/main/kotlin/ch/zuehlke/camp/werewolf/controllers/PlayerProfileController.kt
@@ -5,12 +5,17 @@ import ch.zuehlke.camp.werewolf.domain.Profile
 import ch.zuehlke.camp.werewolf.service.DBFileService
 import ch.zuehlke.camp.werewolf.service.ProfileService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.util.ResourceUtils
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.util.*
+import javax.imageio.ImageIO
 
 @RestController
 @RequestMapping("/api/v1/profile")
@@ -47,7 +52,7 @@ class PlayerProfileController {
         @RequestParam(value = "file", required = false) file: MultipartFile,
         @RequestParam(value = "name", required = false) name: String
     ): ResponseEntity<HttpStatus> {
-        val picture = this.dbFileService.storeFile(file,name)
+        val picture = this.dbFileService.storeFile(file, name)
 
         if (file.isEmpty) {
             return ResponseEntity(HttpStatus.NO_CONTENT)
@@ -60,19 +65,40 @@ class PlayerProfileController {
     }
 
     @GetMapping("/picture/{name}")
-    fun getPicture(@PathVariable name:String): ResponseEntity<ByteArray> {
-        val file = this.dbFileService.getFileFor(name)
-        val responseHeaders = HttpHeaders()
-        responseHeaders.set(
-            HttpHeaders.CONTENT_TYPE,file.contentType
-        )
-        return ResponseEntity
-            .ok()
-            .headers(responseHeaders)
-            .body(file.data!!)
+    fun getPicture(@PathVariable name: String): ResponseEntity<ByteArray> {
+        val file: Picture
+        try {
+            file = this.dbFileService.getFileFor(name)
+            val responseHeaders = HttpHeaders()
 
+            responseHeaders.set(
+                HttpHeaders.CONTENT_TYPE, file.contentType
+            )
+            return ResponseEntity
+                .ok()
+                .headers(responseHeaders)
+                .body(file.data)
+        } catch (e: Exception) {
+            val encodedImage = getEncodedDefaultImage()
+            val defaultResponseHeaders = HttpHeaders()
+            defaultResponseHeaders.set(
+                HttpHeaders.CONTENT_TYPE, "image/jpeg"
+            )
+            return ResponseEntity
+                .ok()
+                .headers(defaultResponseHeaders)
+                .body(encodedImage)
+        }
+    }
 
-
+    private fun getEncodedDefaultImage(): ByteArray? {
+        val file = ResourceUtils.getFile("classpath:profile.jpg")
+        val image = ImageIO.read(file)
+        val outputStream = ByteArrayOutputStream()
+        ImageIO.write(image, "jpg", outputStream)
+        val encodedPicture = outputStream.toByteArray()
+        outputStream.close()
+        return encodedPicture
     }
 
     @PostMapping("")
